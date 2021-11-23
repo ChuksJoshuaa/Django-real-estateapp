74c2557351064e9c5af406a487172c788496422f
mbanaso chukwudi <chuksmbanasoj@gmail.com>
2021-11-23 16:52:24 +0100

    Updated settings

--- Real_Estate/settings.py
+++ Real_Estate/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
-    'bootstrap_modal_forms',
     'cloudinary',
 
      #my_own_app
