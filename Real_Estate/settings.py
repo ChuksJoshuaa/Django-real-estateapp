8dd794d325ea87427014e672146af029bf8ed09b
mbanaso chukwudi <chuksmbanasoj@gmail.com>
2021-12-17 17:25:04 +0100

    Updated files

--- Real_Estate/settings.py
+++ Real_Estate/settings.py
@@ -134,6 +134,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = '/media/'
 
